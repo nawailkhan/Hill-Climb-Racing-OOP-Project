6d9773e1f6f75347529cd669d5be0979d0f18128
javeria-28 <javeriakhan177@gmail.com>
2025-05-06 22:17:08 +0500

    Update Physics.h

--- Physics.h
+++ Physics.h
@@ -1,19 +1,37 @@
 #ifndef PHYSICS_H
+
 #define PHYSICS_H
 
+
+
 class Physics {
+
 public:
+
     static const float GRAVITY;
+
     static const float FRICTION;
+
     static const float AIR_RESISTANCE;
+
     static const float JUMP_FORCE;
+
     static const float ROTATION_DAMPING;
 
+
+
     static void applyGravity(float& velocityY, bool onGround);
+
     static void applyFriction(float& velocityX);
+
     static void applyAirResistance(float& velocityX, float& velocityY);
+
     static bool checkFlipCondition(float angle);
+
     static float calculateAngularAcceleration(float angle);
+
 };
+
+
 
 #endif 
