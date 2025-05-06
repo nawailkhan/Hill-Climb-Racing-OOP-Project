011002b5842855f087f32537c5039a1ff0bd7aa8
javeria-28 <javeriakhan177@gmail.com>
2025-05-06 22:11:55 +0500

    Updated Collectibles.h

--- Collectibles.h
+++ Collectibles.h
@@ -2,23 +2,32 @@
 #define COLLECTIBLE_H
 
 #include "Entity.h"
-
+#include <allegro5/allegro.h> // Add this to define ALLEGRO_BITMAP
+class Game; // Forward declaration
+class Track;
 class Collectible : public Entity {
-private:
+protected:
     bool collected;
 
 public:
-    Collectible(float x, float y, float width, float height)
-        : Entity(x, y, width, height), collected(false) {
-    }
-
-    void update(float collectible) override {
-        // to be updated when a coin or fuel collides with the car
-
-    }
-
+    Collectible(float x, float y, float width, float height);
+    virtual void update(float deltaTime);
+    virtual void render() = 0;
+    virtual void applyEffect(Game& game) = 0;
     void collect() { collected = true; }
     bool isCollected() const { return collected; }
 };
 
+class FuelTank : public Collectible {
+public:
+    FuelTank(float x, float y);
+    ~FuelTank();
+    void update(float deltaTime) override;
+    void render() override;
+    void applyEffect(Game& game) override;
+
+private:
+    ALLEGRO_BITMAP* image = nullptr; // Declare image variable
+};
+FuelTank* SpawnFuelTank(const Track& track);
 #endif
