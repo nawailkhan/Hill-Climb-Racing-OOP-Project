8442cd1573e2ebf54027d22ef98ddf17b1e5546a
javeria-28 <javeriakhan177@gmail.com>
2025-05-06 22:15:19 +0500

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,11 +1,10 @@
 #ifndef GAME_H
 #define GAME_H
-
 #include <allegro5/allegro.h>
 #include "Resources.h"
 #include "Car.h"
 #include "Track.h"
-
+#include "Collectibles.h"
 class Game {
 private:
     ALLEGRO_DISPLAY* display;
@@ -14,27 +13,25 @@ private:
     ALLEGRO_FONT* font;
     ALLEGRO_BITMAP* sky;
     ALLEGRO_BITMAP* car_image;
-
     bool running;
     bool gameOverShown;
     bool key[ALLEGRO_KEY_MAX];
-
     float cameraX;
     float fuel;
     Car car;
     Track track;
-
+    FuelTank* fuelTank = nullptr;
+    
     void initialize();
     void handleEvents();
     void update();
     void render();
     void cleanUp();
-
 public:
     Game();
+    void addFuel(float amount);
     ~Game();
     void run();
 };
-
 #endif 
- 
+
