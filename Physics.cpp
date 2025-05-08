ab58e56e553e08950f7924638b98000bba6d4dcd
nawail khan <141786562+nawailkhan@users.noreply.github.com>
2025-05-08 22:36:15 +0500

    Update Physics.cpp

--- Physics.cpp
+++ Physics.cpp
@@ -6,6 +6,8 @@ const float Physics::FRICTION = 0.030f;
 const float Physics::AIR_RESISTANCE = 0.99f;
 const float Physics::JUMP_FORCE = -15.0f;
 const float Physics::ROTATION_DAMPING = 0.93f;
+const float Physics::HILL_CLIMB_RESISTANCE = 0.25f;  
+const float Physics::HILL_DESCEND_BOOST = 0.09f;
 
 void Physics::applyGravity(float& velocityY, bool onGround) {
     if (!onGround) {
@@ -20,9 +22,21 @@ void Physics::applyFriction(float& velocityX) {
     }
 }
 
-void Physics::applyAirResistance(float& velocityX, float& velocityY) {
+void Physics::applyAirResistance(float& velocityX, float& velocityY, float slopeAngle) {
     velocityX *= AIR_RESISTANCE;
     velocityY *= AIR_RESISTANCE;
+
+    float slopeEffect = sin(slopeAngle * ALLEGRO_PI / 180.0f);
+
+    if (slopeEffect < 0) {
+        float resistance = std::abs(slopeEffect) * HILL_CLIMB_RESISTANCE;
+        velocityX *= (1.0f - resistance);
+    }
+   
+    else {
+        velocityX += slopeEffect * HILL_DESCEND_BOOST; 
+    }
+    if (std::abs(velocityX) < 0.1f) velocityX = 0;
 }
 
 bool Physics::checkFlipCondition(float angle) {
